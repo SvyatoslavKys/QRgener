25cd196345d033ef1c8eacadd7891078577ed398
SvyatoslavKys <158611727+SvyatoslavKys@users.noreply.github.com>
2025-07-05 11:37:38 +0200

    style

--- src/GenerateHistory.jsx
+++ src/GenerateHistory.jsx
@@ -4,7 +4,7 @@ export const GenerateHistory = () => {
     const datag = JSON.parse(localStorage.getItem(GENERATE_DATE) || "[]");
 
     return (
-        <div className='mt-4 flex justify-center flex-col gap-4 text-blue-600'>
+        <div className='mt-4 ml-4 flex justify-center flex-col gap-4 text-blue-600'>
             {datag.map((text) => (
                 <p key={text}>{text}</p>
             ))}
--- src/ScanHistory.jsx
+++ src/ScanHistory.jsx
@@ -4,7 +4,7 @@ import { SCAN_DATA } from './constants';
 export const ScanHistory = () => {
     const data = JSON.parse(localStorage.getItem(SCAN_DATA) || "[]");
     return(
-        <div className='mt-4 flex justify-center flex-col gap-4 text-blue-600' >
+        <div className='mt-4 ml-4 flex justify-center flex-col gap-4 text-blue-600' >
             {data.map((text )=> (
                 <a target="_blank" href={text}><p className="hover:text-red-500" key={text}>{text}</p></a>
                ))
