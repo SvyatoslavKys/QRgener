0cc4e14e5774bcdff25355d5fb4ed8a100a1a85e
SvyatoslavKys <svyat.ne@gmail.com>
2025-11-05 23:30:08 +0100

    addres

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/QRgener/', // это имя репозитория (точно как на GitHub!)
+  base: '/QRgener/', 
 })
