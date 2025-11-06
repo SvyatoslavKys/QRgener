fd04b1e2dc6a7989558a4d5fb68f36192432d51e
SvyatoslavKys <svyat.ne@gmail.com>
2025-11-06 09:00:30 +0100

    icon

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -4,7 +4,7 @@ import qrlogo from "../public/qrlogo.svg";
 export const Navigation = () => {
   return (
     <nav className="flex justify-center items-start gap-4 p-4 bg-gray-200">
-      <Link to="/QRgener" className="text-[13px] md:text-base lg:text-lg text-blue-600 hover:text-red-600"><img src={qrlogo} alt="QR logo" className="w-8 h-8" /></Link>
+      <Link to="/QRgener" className="text-[13px]transition-transform duration-300 ease-in-out hover:scale-110 "><img src={qrlogo} alt="QR logo" className="w-8 h-8" /></Link>
       <Link to="/scan" className="text-[13px] md:text-base lg:text-lg text-blue-600 hover:text-red-600">Scan QR</Link>
       <Link to="/generate" className="text-[13px] md:text-base lg:text-lg  text-blue-600 hover:text-red-600">Generate QR</Link>
       <Link to="/scanHistory" className="text-[13px] md:text-base lg:text-lg  text-blue-600 hover:text-red-600">Scan history</Link>
